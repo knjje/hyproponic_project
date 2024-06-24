52e26a487a511aee3c73c273c31738e9f132fda6
natthanicha.j <natthanicha.j@adevsolutions.com>
2024-06-25 00:06:46 +0700

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production:true,
-    endpoint: '',
+    production: false,
+    endpoint: 'http://localhost:3000/',
     firebase: {
         apiKey: "AIzaSyDC4RoIxtD36gbhFYJeCGEPFXgqDbhk2Cw",
         authDomain: "test-esp32-14072.firebaseapp.com",
