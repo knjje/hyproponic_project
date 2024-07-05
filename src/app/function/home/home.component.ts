048d1afdb6cd58daedbe878eecf3b9b982588114
natthanicha.j <natthanicha.j@adevsolutions.com>
2024-07-05 18:18:38 +0700

    select day to plot graph

--- src/app/function/home/home.component.ts
+++ src/app/function/home/home.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import {
+  NativeDateAdapter,
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+} from '@angular/material/core';
+import { MAT_NATIVE_DATE_FORMATS } from '@angular/material/core';
 import { AuthService } from 'src/app/service/auth.service';
-import { environment } from 'src/environments/environment';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { Chart, registerables } from 'chart.js';
+import { DatePipe } from '@angular/common';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
+  providers: [DatePipe],
 })
 export class HomeComponent implements OnInit {
   humidityValue: any;
@@ -17,24 +31,58 @@ export class HomeComponent implements OnInit {
   pump_ph_down: any;
   pump_ph_up: any;
   pump_water_up: any;
-  data: any;
+  data: any = [];
   tempChart: any;
   humiChart: any;
   phChart: any;
-
-  constructor(private auth: AuthService, private db: AngularFireDatabase) {
+  body: any = {
+    startDate: null,
+    // endDate: null,
+  };
+  constructor(
+    private auth: AuthService,
+    private db: AngularFireDatabase,
+    private datePipe: DatePipe
+  ) {
     Chart.register(...registerables);
   }
 
   ngOnInit(): void {
-    this.getLog();
     this.getdata();
+    this.getLog();
+    this.test();
   }
 
-  async getLog() {
+  async test() {
     try {
       let res: any = await this.auth.Get('getLog');
       this.data = res.data;
+      this.createCharts();
+    } catch (error) {
+      console.error('Error fetching log data:', error);
+    }
+  }
+
+  async getLog(): Promise<void> {
+    if (!this.body.startDate) {
+      this.body.startDate = new Date();
+      this.body.startDate = this.datePipe.transform(
+        this.body.startDate,
+        'yyyy-MM-dd'
+      );
+    } else if (this.body.startDate instanceof Date) {
+      this.body.startDate = this.datePipe.transform(
+        this.body.startDate,
+        'yyyy-MM-dd'
+      );
+    }
+
+    console.log('Formatted Date Range:', this.body);
+    try {
+      let res: any = await this.auth.Post('filLog', this.body);
+      console.log('Response from API:', res);
+
+      this.data = res.data; // Assuming res contains the entire response object with data property
       this.createCharts();
     } catch (error) {
       console.error('Error fetching log data:', error);
